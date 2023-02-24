08314b0f367a5800a397acc280fbb6c1e1df7ba1
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-02-24 18:40:15 +0530

    task7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ExpenseItem() {
+    return (
+        <>
+            <h3>Expenses Item</h3>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movie Rs 200</p>
+        </>
+    )
+}
+
+export default ExpenseItem;
