1b9bbdf18758b83a0391c55b078a6a1e6575576c
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-02-25 16:27:55 +0530

    task9:propsLooping-.map

--- src/App.js
+++ src/App.js
@@ -28,10 +28,16 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItem>
+      {
+        expenses.map((expense) =>
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            location={expense.location}>
+          </ExpenseItem>
+        )
+      }
     </div>
   );
 
