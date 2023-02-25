7191fcdc36137aeeb35fdefdd8d153c551baa268
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-02-25 12:33:13 +0530

    task8:ExpenseLocationAdded

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,16 +5,18 @@ function ExpenseItem() {
     const expenseDate = new Date(2021, 2, 28);
     const expenseTitle = 'Car Insurance';
     const expenseAmount = 294.67;
+    const expenseLocation = 'Mumbai';
 
     return (
         <div className='expense-item'>
             <div>{expenseDate.toDateString()}</div>
             <div className='expense-item__description'>
+                <div style={{ color: "red" }}>{expenseLocation}</div>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
             </div>
 
-        </div>
+        </div >
     )
 }
 
