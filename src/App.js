da45b74c1d9225f9507016b89db9e12501982376
rishii-27 <hrishikesh.joshi1997@gmail.com>
2023-02-28 21:45:55 +0530

    task12: OrganisedWay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+// import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 function App() {
   const expenses = [
